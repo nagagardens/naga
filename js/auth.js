05a215ea113a82e7558e0b4be182f55b944d7bcd
lfernandogonzalez <gofernando83@gmail.com>
2023-02-16 12:15:27 -0500

    new site

---
+++ js/auth.js
@@ -0,0 +1,142 @@
+// JavaScript File
+
+
+function signInButton() {
+      
+  var data = { 
+    UserPoolId : _config.cognito.userPoolId,
+    ClientId : _config.cognito.clientId
+  };
+
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+  var cognitoUser = userPool.getCurrentUser();
+
+  var authenticationData = {
+    Username : document.getElementById("inputUsername").value,
+    Password : document.getElementById("inputPassword").value,
+  };
+
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+  
+  var userData = {
+    Username : document.getElementById("inputUsername").value,
+    Pool : userPool,
+  };
+  
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  
+  cognitoUser.authenticateUser(authenticationDetails, {
+    
+    onSuccess: function (result) {
+
+      var accessToken = result.getAccessToken().getJwtToken();
+      console.log(result);	
+      
+      cognitoUser.getUserAttributes(function(err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        window.location.href='./profile.html';
+    
+      });
+
+    },
+      onFailure: function(err) {
+        alert(err.message || JSON.stringify(err));
+    },
+  });
+      
+}
+
+
+async function getUser(email_address) {
+  
+  // gets user from DynamoDB using email address  
+  const api_url = 'https://thv3sn3j63.execute-api.us-east-1.amazonaws.com/prod/get_naga_user_by_email?user_email=' + encodeURIComponent(email_address);
+  const api_response = await fetch(api_url);
+  const api_data = await(api_response).json();
+  console.log(api_data);
+
+  var name = JSON.parse(api_data['body'])['name'];
+  
+  document.getElementById('profile').innerHTML =  name;
+  document.getElementById('sign-out').style.display = "block";
+  document.getElementById('loader').style.display = "none";
+
+  }
+
+function getUserAttributes() {
+  var data = {
+    UserPoolId : _config.cognito.userPoolId,
+    ClientId : _config.cognito.clientId
+  };
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+  var cognitoUser = userPool.getCurrentUser();
+
+  if (cognitoUser != null) {
+    cognitoUser.getSession(function(err, session) {
+      if (err) {
+        alert(err);
+        return;
+      }
+      //console.log('session validity: ' + session.isValid());
+
+      cognitoUser.getUserAttributes(function(err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        // user email address
+        console.log(result[2].getValue());
+        getUser(result[2].getValue())
+      });
+
+    });
+  } else {
+    console.log("Already signed-out")
+    document.getElementById('sign-out').style.display = "none";
+    document.getElementById('loader').style.display = "none";
+    document.getElementById('sign-in').style.display="block";
+  }
+}
+
+
+
+function signOut() {
+
+  document.getElementById('loader').style.display = "block";
+  document.getElementById('sign-out').style.display = "none"
+  
+  const data = { 
+      UserPoolId : _config.cognito.userPoolId,
+    ClientId : _config.cognito.clientId
+  };
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+  const cognitoUser = userPool.getCurrentUser();
+  
+    if (cognitoUser != null) {
+      cognitoUser.getSession(function(err, session) {
+      if (err) {
+          alert(err);
+        return;
+      }
+      console.log('session validity: ' + session.isValid());
+      
+            // sign out
+            cognitoUser.signOut();
+            console.log("Sign out successful");
+            
+      });
+    } else {
+        console.log("Already signed-out")
+    }
+    window.location.href='./index.html';
+    
+}
+
+window.onload = function(){
+  const temp_var = getUserAttributes();
+}
+
--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_FnA8aL4iX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '283tlnkvqd5sv6hcutv82487vv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_cd4B1n2Hv', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '6mkmj7cfc7vd5g04cgm6lrm6ql' //is this used anywhere?
     },
-    api: {
-        invokeUrl: 'https://4l2kjimrx0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
 };
